2a15cfac9ac151ed110ff78ad01ff4d181a835da
ZJ Chin <zhejingchin@gmail.com>
2023-08-22 10:38:06 +0100

    update read_pkl to realign data

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,18 @@
 import os
 from read_subject import get_personal_information
-from read_sensor import get_respiban, read_empatica_csv, read_pkl, read_quest_csv
+from read_sensor import read_pkl, read_quest_csv
+
+""" 
+Problems definition: 
+1. To create a data processing pipeline for WESAD sensor data. 
+2. The resulting datasets should be in consistent format. (Same column names and types and order)
+3. The end output helps in easier analysing and using for building predictive mood model (classification).
+4. The processed data should be clear and full with details to provide greater space for feature engineering / extraction.
+
+Git repo: https://github.com/Zhejing-Chin/OnePlanet
+Functions in separate files for easier management and code reusabiltiy. 
+"""
+
 
 path = "./WESAD"
 subjects = next(os.walk(path))[1]
--- Code/read_sensor.py
+++ Code/read_sensor.py
@@ -5,153 +5,181 @@ import os
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
+from scipy.signal import resample, decimate
+from scipy.interpolate import interp1d
 
-
-
-def read_respiban_txt(dataFolder, subject):
-    vcc=3
-    chan_bit=2**16
-    Cmin = 28000 
-    Cmax = 38000
+# def read_respiban_txt(dataFolder, subject):
+#     vcc=3
+#     chan_bit=2**16
+#     Cmin = 28000 
+#     Cmax = 38000
     
-    path = f"{dataFolder}/{subject}/{subject}_respiban.txt"
-    rows = []
-    with open(path) as f:    
-        next(f)
-        next(f)
-        # header = json.loads(f.readline().strip()[2:])
-        next(f)    
-        for line in f:
-            values = line.strip().split()
+#     path = f"{dataFolder}/{subject}/{subject}_respiban.txt"
+#     rows = []
+#     with open(path) as f:    
+#         next(f)
+#         next(f)
+#         # header = json.loads(f.readline().strip()[2:])
+#         next(f)    
+#         for line in f:
+#             values = line.strip().split()
             
-            ecg = ((float(values[2])/chan_bit-0.5)*vcc)
-            eda = (((float(values[3])/chan_bit)*vcc)/0.12)
-            emg = ((float(values[4])/chan_bit-0.5)*vcc)
-            vout = (float(values[5])*vcc)/(chan_bit-1.) 
-            rntc = ((10**4)*vout)/(vcc-vout) 
-            # assume log base e
-            temp = -1
-            try:
-                temp = - 273.15 + 1./(1.12764514*(10**(-3)) + 2.34282709*(10**(-4))*math.log(rntc) + 8.77303013*(10**(-8))*(math.log(rntc)**3))
-            except ValueError:
-                print(values[0], rntc)
-            xyz = [(float(x)-Cmin)/(Cmax-Cmin)*2-1 for x in values[6:9]]
-            resp = (float(values[9]) / chan_bit - 0.5) * 100
+#             ecg = ((float(values[2])/chan_bit-0.5)*vcc)
+#             eda = (((float(values[3])/chan_bit)*vcc)/0.12)
+#             emg = ((float(values[4])/chan_bit-0.5)*vcc)
+#             vout = (float(values[5])*vcc)/(chan_bit-1.) 
+#             rntc = ((10**4)*vout)/(vcc-vout) 
+#             # assume log base e
+#             temp = -1
+#             try:
+#                 temp = - 273.15 + 1./(1.12764514*(10**(-3)) + 2.34282709*(10**(-4))*math.log(rntc) + 8.77303013*(10**(-8))*(math.log(rntc)**3))
+#             except ValueError:
+#                 print(values[0], rntc)
+#             xyz = [(float(x)-Cmin)/(Cmax-Cmin)*2-1 for x in values[6:9]]
+#             resp = (float(values[9]) / chan_bit - 0.5) * 100
             
-            rows.append({'id':subject, 
-            'seq':values[0],
-            'ecg':ecg, 
-            'eda':eda, 
-            'emg':emg, 
-            'temp':temp,
-            'xyz_1':xyz[0],       
-            'xyz_2':xyz[1], 
-            'xyz_3':xyz[2], 
-            'respiration':resp})
+#             rows.append({'id':subject, 
+#             'seq':values[0],
+#             'ecg':ecg, 
+#             'eda':eda, 
+#             'emg':emg, 
+#             'temp':temp,
+#             'xyz_1':xyz[0],       
+#             'xyz_2':xyz[1], 
+#             'xyz_3':xyz[2], 
+#             'respiration':resp})
         
-    return rows
+#     return rows   
         
-        
-def get_respiban(dataFolder, subjects):
-    respiban = pd.DataFrame({'id':'', 
-                             'seq':int(),
-                             'ecg':float(), 
-                             'eda':float(), 
-                             'emg':float(), 
-                             'temp':float(),
-                             'xyz_1':float(),       
-                             'xyz_2':float(), 
-                             'xyz_3':float(), 
-                             'respiration':float()}, index=[])
+# def get_respiban(dataFolder, subjects):
+#     respiban = pd.DataFrame({'id':'', 
+#                              'seq':int(),
+#                              'ecg':float(), 
+#                              'eda':float(), 
+#                              'emg':float(), 
+#                              'temp':float(),
+#                              'xyz_1':float(),       
+#                              'xyz_2':float(), 
+#                              'xyz_3':float(), 
+#                              'respiration':float()}, index=[])
     
-    for subject in subjects:
-        rows = read_respiban_txt(dataFolder, subject)
+#     for subject in subjects:
+#         rows = read_respiban_txt(dataFolder, subject)
         
-        respiban = respiban.append(rows,
-            ignore_index = True)
+#         respiban = respiban.append(rows,
+#             ignore_index = True)
         
-    return respiban
+#     return respiban
 
-def unzip_empatica(dataFolder, subject):
-    path = f"{dataFolder}/{subject}"
-    # print(path)
-    check_unzipped = next(os.walk(path))[1]
-    if not f"{subject}_E4_Data" in check_unzipped:
-        zipfile = f"{dataFolder}/{subject}/{subject}_E4_Data.zip"
-    # print(path)
-        with ZipFile(zipfile, 'r') as zObject:
-            zObject.extractall(path+f"/{subject}_E4_Data")
+# --------------------
+# not necessary to read empatica, as it must be manually synchronized with respiban to get labels
+# pkl is already providing synchronized data
+# --------------------
+# def unzip_empatica(dataFolder, subject):
+#     path = f"{dataFolder}/{subject}"
+#     # print(path)
+#     check_unzipped = next(os.walk(path))[1]
+#     if not f"{subject}_E4_Data" in check_unzipped:
+#         zipfile = f"{dataFolder}/{subject}/{subject}_E4_Data.zip"
+#     # print(path)
+#         with ZipFile(zipfile, 'r') as zObject:
+#             zObject.extractall(path+f"/{subject}_E4_Data")
             
-def read_empatica_csv(dataFolder, subject):
-    # make sure folder is unzipped
-    unzip_empatica(dataFolder, subject)
+# def read_empatica_csv(dataFolder, subject):
+#     # make sure folder is unzipped
+#     unzip_empatica(dataFolder, subject)
     
-    csv_files = ['ACC', 'BVP', 'EDA', 'TEMP']
+#     csv_files = ['ACC', 'BVP', 'EDA', 'TEMP']
     
 def read_pkl(dataFolder, subject):
+    # pkl file is already synchronized
+    # only need to realign data rows by up / downsampling
+    
     path = f"{dataFolder}/{subject}/{subject}.pkl"
     sync = pd.read_pickle(path) 
+    
     # ACC, ECG, EDA, EMG, Resp, Temp 
-    acc = [list(x) for x in zip(*sync['signal']['chest']['ACC'])]
-    length = len(acc[0])
-    # print(length)
-    # print(sync['signal']['chest'].keys())
-    chest = {'xyz_1': acc[0],
-             'xyz_2': acc[1],
-             'xyz_3': acc[2],
-             'ecg': sync['signal']['chest']['ECG'].reshape(length),
-             'emg': sync['signal']['chest']['EMG'].reshape(length),
-             'eda': sync['signal']['chest']['EDA'].reshape(length),
-             'resp': sync['signal']['chest']['Resp'].reshape(length),
-             'temp': sync['signal']['chest']['Temp'].reshape(length),
+    chest_acc = sync['signal']['chest']['ACC'].astype(float)
+    
+    length = chest_acc.shape[0]
+    chest = {'xyz_1': chest_acc[:, 0],
+             'xyz_2': chest_acc[:, 1],
+             'xyz_3': chest_acc[:, 2],
+             'ecg': sync['signal']['chest']['ECG'].reshape(length).astype(float),
+             'emg': sync['signal']['chest']['EMG'].reshape(length).astype(float),
+             'eda': sync['signal']['chest']['EDA'].reshape(length).astype(float),
+             'resp': sync['signal']['chest']['Resp'].reshape(length).astype(float),
+             'temp': sync['signal']['chest']['Temp'].reshape(length).astype(float),
              'label': sync['label']}
-    # print(chest)
     respiban = pd.DataFrame.from_dict(chest)
     respiban.insert(loc=0, column='id', value=subject)
-    
-    acc_chest = acc
 
     
     # ACC, BVP, EDA, TEMP
-    acc = [list(map(float, x)) for x in zip(*sync['signal']['wrist']['ACC'])]
-    # acc[0] = 
+    # acc at 1/64g
+    wrist_acc = sync['signal']['wrist']['ACC'].astype(float) * (1/64)
+    
+    # ------------------------------
+    # * perform chest and wrist realignement first, as accelerometer data provides the timing information that can be used as a reference for synchronizing multimodal.
+    # downsample chest data to match with wrist data
+    # reason: to cope with lack of memory
+    # risk: losing information
+    # solution: upsample wrist data instead as chest data has more points to match with
+    # -------------------------------
+    resampling_factor = 32/700
+    num_samples_after_resample = int(len(chest_acc) * resampling_factor)
+    chest_acc_32hz = resample(chest_acc, num_samples_after_resample, axis=0)
+    
+    print(wrist_acc.shape, chest_acc_32hz.shape)
+    
     #64hz
-    bvp = sync['signal']['wrist']['BVP'].reshape(sync['signal']['wrist']['BVP'].size)
+    wrist_bvp = sync['signal']['wrist']['BVP'].reshape(sync['signal']['wrist']['BVP'].size).astype(float)
     #4hz
-    eda = sync['signal']['wrist']['EDA'].reshape(sync['signal']['wrist']['EDA'].size)
-    temp = sync['signal']['wrist']['TEMP'].reshape(sync['signal']['wrist']['TEMP'].size)
-    # print(eda.size, bvp.size, temp.size)
+    wrist_eda = sync['signal']['wrist']['EDA'].reshape(sync['signal']['wrist']['EDA'].size).astype(float)
+    wrist_temp = sync['signal']['wrist']['TEMP'].reshape(sync['signal']['wrist']['TEMP'].size).astype(float)
     
-    acc = [np.repeat(i, repeats=2, axis=0) * (1/64) for i in acc]
-    eda = np.repeat(eda, repeats=16, axis=0)
-    temp = np.repeat(temp, repeats=16, axis=0)
+    # --------------
+    # since chest and wrist aligned, realign the other features to match 32hz to ensure consistency
+    # decimate bvp, interpolate eda and temp
+    # it might introduces artifacts
+    # could try to resample with different sampling rate.
+    # ---------------
     
-    print(acc[0].size, eda.size, bvp.size, temp.size)
-    wrist = {'xyz_1': acc[0],
-             'xyz_2': acc[1],
-             'xyz_3': acc[2],
-             'bvp': bvp,
-             'eda': eda,
-             'temp': temp}
-    empatica = pd.DataFrame.from_dict(wrist)
-    empatica.insert(loc=0, column='id', value=subject)
+    # decimate bvp
+    wrist_bvp_32hz = decimate(wrist_bvp, q=2, ftype='iir')
     
-    acc_wrist = acc
-    # plot_acceleration(acc_chest, acc_wrist)
+    # interpolate eda, temp
+    target_sampling_rate = 32  # Hz
+    num_samples = len(wrist_acc)
+    time_indices = np.arange(num_samples) / target_sampling_rate
+    wrist_eda_32hz = np.interp(time_indices, np.arange(len(wrist_eda)) / 4, wrist_eda)
+    wrist_temp_32hz = np.interp(time_indices, np.arange(len(wrist_temp)) / 4, wrist_eda)
+
+    assert wrist_acc.shape[0]==wrist_bvp_32hz.shape[0]\
+         & wrist_acc.shape[0]==wrist_eda_32hz.shape[0]\
+         & wrist_acc.shape[0]==wrist_temp_32hz.shape[0]
     
-    cross_corr_result = calculate_cross_correlation(acc_chest, acc_wrist)
-    max_correlation_index = np.argmax(cross_corr_result)
-    time_offset = max_correlation_index - (len(acc_chest) - 1)
+    # wrist = {'xyz_1': acc[0],
+    #          'xyz_2': acc[1],
+    #          'xyz_3': acc[2],
+    #          'bvp': bvp,
+    #          'eda': eda,
+    #          'temp': temp}
+    # empatica = pd.DataFrame.from_dict(wrist)
+    # empatica.insert(loc=0, column='id', value=subject)
+    
+    
+    
+    
+    
+
+    # print(resampled_chest_data.shape, acc_wrist.shape)
+    
+    # plot_acceleration(resampled_chest_data.transpose(), acc_wrist.transpose())
     
     # assume label 4 link to both med 1 and med 2 in ground truth
     
-    return respiban, empatica
-
-def calculate_cross_correlation(signal_x, signal_y):
-    cross_corr = np.correlate(signal_x, signal_y, mode='full')
-    return cross_corr
-
+    return respiban, None #empatica
 
 
 def plot_acceleration(acc_chest, acc_wrist):
